855304108fa72c441f9d040605d0cb4c7ff39b4a
Alisia-Nadia <sarbalisia@yahoo.com>
2017-05-12 16:19:31 +0300

    Update SalesAssistant.java

--- src/Model/SalesAssistant.java
+++ src/Model/SalesAssistant.java
@@ -2,9 +2,29 @@ package Model;//Magnus Svendsen DAT16i
 
 public class SalesAssistant extends Employee
 {
-    @Override
-    public String toString()
-    {
-        return "Sales Assistant";
+    
+    private String positionName;
+
+    public SalesAssisten(String name, int cpr, Date dateOfBirth, String address, int phoneNumber, String email, String positionName){
+
+        super(name,cpr,dateOfBirth,address, phoneNumber,email);
+        positionName = this.positionName;
+
     }
+
+    public String getPositionName() {
+        return positionName;
+    }
+
+    public void setPositionName(String positionName) {
+        this.positionName = positionName;
+    }
+
+    public String toSting(){
+
+        return super.toString() +
+                "\nPosition:" + this.positionName;
+
+    }
+
 }
