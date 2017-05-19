a6bb027c1ce6d9f33ab109bcb58b9de984f9273e
Magnus <magnumizer@gmail.com>
2017-05-19 16:04:21 +0200

    changed login
    -removed position
    -changed automechanic gui names to service (because we are adding
    cleaning service)
    -added cleaning staff class

--- src/Controller/LController.java
+++ src/Controller/LController.java
@@ -17,17 +17,7 @@ import java.util.ResourceBundle;
 
 public class LController implements Initializable
 {
-
-    ObservableList<String> positions =
-            FXCollections.observableArrayList(
-                    "Admin",
-                    "Sales Assistant",
-                    "Auto Mechanic"
-            );
-
     //region FXML
-    @FXML
-    private ComboBox<String> positionBox;
     @FXML
     private TextField nameField;
     @FXML
@@ -39,43 +29,36 @@ public class LController implements Initializable
 
     public void onLoginBtnPressed(ActionEvent actionEvent)
     {
-        if (!positionBox.getSelectionModel().isEmpty())
+        if (!nameField.getText().equals(""))
         {
-            if (!nameField.getText().equals(""))
+            if (!passwordField.getText().equals(""))
             {
-                if (!passwordField.getText().equals(""))
+                if (loginHandler.handleLogin(nameField.getText(), passwordField.getText()))
                 {
-                    if (loginHandler.handleLogin(positionBox.getValue(), nameField.getText(), passwordField.getText()))
-                    {
-                        //if password is true/correct, move onto next screen based on position
-                        System.out.println("Password accepted.");
-                        stageHandler.handleSceneChange(positionBox.getValue());
-                    }
-                    else
-                    {
-                        //if password is false/incorrect, clear the password field, notify user
-                        if (Main.primaryStage.isShowing())
-                        {
-                            passwordField.clear();
-                            stageHandler.displayError("Error logging in", "One or more fields are incorrect", "Please try again");
-                        }
-                    }
+                    //if password is true/correct, move onto next screen based on position
+                    System.out.println("Password accepted.");
+                    stageHandler.handleSceneChange();
                 }
                 else
                 {
-                    stageHandler.displayError("Password not specified", "Password can't be blank", "Please enter a valid password");
-                    passwordField.requestFocus();
+                    //if password is false/incorrect, clear the password field, notify user
+                    if (Main.primaryStage.isShowing())
+                    {
+                        passwordField.clear();
+                        stageHandler.displayError("Error logging in", "One or more fields are incorrect", "Please try again");
+                    }
                 }
             }
             else
             {
-                stageHandler.displayError("Name not specified", "Name is missing", "Please enter a username");
-                nameField.requestFocus();
+                stageHandler.displayError("Password not specified", "Password can't be blank", "Please enter a valid password");
+                passwordField.requestFocus();
             }
         }
         else
         {
-            stageHandler.displayError("Position not selected", "Position is missing", "Please select a position");
+            stageHandler.displayError("Name not specified", "Name is missing", "Please enter a username");
+            nameField.requestFocus();
         }
     }
 
@@ -88,8 +71,6 @@ public class LController implements Initializable
     @Override
     public void initialize(URL location, ResourceBundle resources)
     {
-        positionBox.setItems(positions);
-
         //get data from db later, for now add here
 
         if (!Employee.checkIfUserExists("test0"))
---
+++ src/Model/CleaningPersonnel.java
@@ -0,0 +1,11 @@
+package Model;//Magnus Svendsen DAT16i
+
+import java.time.LocalDate;
+
+public class CleaningPersonnel extends Employee
+{
+    public CleaningPersonnel(String name, String cpr, LocalDate dateOfBirth, String address, int phoneNumber, String email, String username, String password)
+    {
+        super(name, cpr, dateOfBirth, address, phoneNumber, email, username, password);
+    }
+}
